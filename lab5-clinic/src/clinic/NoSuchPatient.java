3c13ddda352040a98165ce6c78714b242882d8b4
Mert Ozcan <93086504+Pierciest@users.noreply.github.com>
2024-06-28 02:41:42 +0200

    Delete lab5-clinic directory

--- lab5-clinic/src/clinic/NoSuchPatient.java
+++
@@ -1,9 +0,0 @@
-package clinic;
-
-
-/**
- * Exception class used to notify that a Patient is not available
- */
-public class NoSuchPatient extends Exception {
-	private static final long serialVersionUID = 1L;
-}
